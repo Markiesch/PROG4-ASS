9404ada7736f140b7c8119a3e09b7512732d0a55
Markiesch <marksmeel@gmail.com>
2024-06-09 12:34:30 +0200

    Kleur parameter toegevoegd

--- src/nl/markschuurmans/painting/model/TreeSize.java
+++ src/nl/markschuurmans/painting/model/TreeSize.java
@@ -1,9 +1,21 @@
 package nl.markschuurmans.painting.model;
 
+import javafx.scene.paint.Color;
+
 public enum TreeSize {
-    S,
-    M,
-    L,
-    XL,
-    XXL
+    S(Color.LIME),
+    M(Color.LIMEGREEN),
+    L(Color.LIGHTGREEN),
+    XL(Color.GREEN),
+    XXL(Color.DARKGREEN);
+
+    private final Color color;
+
+    TreeSize(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
