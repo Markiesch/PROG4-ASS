b119f74847cd34e0e51f8a3143290b08ec858ac3
Markiesch <marksmeel@gmail.com>
2024-06-09 13:47:22 +0200

    Getters en setters toegevoegd

--- src/nl/markschuurmans/painting/model/Tree.java
+++ src/nl/markschuurmans/painting/model/Tree.java
@@ -7,4 +7,37 @@ public class Tree {
     private double relY;
 
     public void move() {}
+
+    public TreeSize getSize() {
+        return size;
+    }
+
+    public void setSize(TreeSize size) {
+        this.size = size;
+    }
+
+    public TreeType getType() {
+        return type;
+    }
+
+    public void setType(TreeType type) {
+        this.type = type;
+    }
+
+    public double getRelX() {
+        return relX;
+    }
+
+    public void setRelX(double relX) {
+        this.relX = relX;
+    }
+
+    public double getRelY() {
+        return relY;
+    }
+
+    public void setRelY(double relY) {
+        this.relY = relY;
+    }
+
 }
